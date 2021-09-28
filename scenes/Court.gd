26e4ccd87194598a4ef1ac6a6634c150ac797762
Sócrates Díaz <sdsxd1800@gmail.com>
2021-09-27 22:23:17 -0400

    Add point scoring system and game restart.

--- scenes/Court.gd
+++ scenes/Court.gd
@@ -2,6 +2,27 @@ class_name Court
 extends Node2D
 
 onready var ball = $Ball
+onready var p1ScoreLabel = $P1Score
+onready var p2ScoreLabel = $P2Score
+onready var restartTimer = $RestartGameTimer
+onready var ballSpawnPosition = $BallSpawn
 
-func _ready():
-	pass
+var scoreP1 = 0
+var scoreP2 = 0
+	
+func _process(delta):
+	p1ScoreLabel.text = str(scoreP1)
+	p2ScoreLabel.text = str(scoreP2)
+
+func _on_LeftLimit_body_entered(body):
+	if body is Ball:
+		scoreP2 += 1
+		restartTimer.start()
+
+func _on_RightLimit_body_entered(body):
+	if body is Ball:
+		scoreP1 += 1
+		restartTimer.start()
+
+func _on_RestartGameTimer_timeout():
+	ball.position = ballSpawnPosition.position
