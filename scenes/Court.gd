83339fa60e05225f9be1e6de89c48476b32e2d4f
Sócrates Díaz <sdsxd1800@gmail.com>
2021-10-08 22:10:58 -0400

    Add results screen after finishing a match.

--- scenes/Court.gd
+++ scenes/Court.gd
@@ -9,6 +9,9 @@ onready var ballSpawnPosition = $BallSpawn
 onready var p1Paddle = $LeftPlayerPaddle
 onready var p2Paddle = $RightPlayerPaddle
 onready var pauseScreen = $PauseScreen
+onready var resultScreen = $ResultScreen
+
+const MAX_SCORE := 5
 
 var scoreP1 := 0
 var scoreP2 := 0
@@ -19,11 +22,11 @@ func _process(_delta) -> void:
 
 func _on_LeftLimit_body_entered(_body: Ball) -> void:
 	scoreP2 += 1
-	restartTimer.start()
+	_calculate_result_and_show(scoreP2, "P2")
 
 func _on_RightLimit_body_entered(_body: Ball) -> void:
 	scoreP1 += 1
-	restartTimer.start()
+	_calculate_result_and_show(scoreP1, "P1")
 
 func _on_RestartGameTimer_timeout() -> void:
 	ball.position = ballSpawnPosition.position
@@ -42,3 +45,12 @@ func _on_LeftPaddleAIArea_body_exited(_body: Ball) -> void:
 
 func _on_RightPaddleAIArea_body_exited(_body: Ball) -> void:
 	p2Paddle.ball = null
+
+func _calculate_result_and_show(score: int, player_name: String) -> void:
+	if score >= MAX_SCORE:
+		resultScreen.player_name = player_name
+		resultScreen.visible = true
+		get_tree().set_deferred("paused", true)
+		pauseScreen.set_process_input(false)
+	else:
+		restartTimer.start()
---
+++ scenes/ResultScreen.gd
@@ -0,0 +1,17 @@
+class_name ResultScreen
+extends Control
+
+onready var globals = $"/root/Globals"
+onready var resultLabel = $Control/Result
+
+var player_name = ""
+
+func _process(delta):
+	resultLabel.text = player_name + " WON"
+
+func _on_PlayAgainBtn_pressed():
+	get_tree().reload_current_scene()
+
+
+func _on_MainMenuBtn_pressed():
+	globals.show_main_menu()
